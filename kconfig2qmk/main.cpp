60ccc26a516dca034746a5a6066c7b6b857da848
Jean-Francois Dagenais <jeff.dagenais@gmail.com>
2019-09-12 09:33:37 -0400

    kconfig2qmk: add parsing of autoconf.mk
    Also now supports qt5

--- kconfig2qmk/main.cpp
+++ kconfig2qmk/main.cpp
@@ -1,4 +1,4 @@
-#include <QtCore/QCoreApplication>
+#include <QCoreApplication>
 #include <QFile>
 #include <QDebug>
 #include <QStringList>
@@ -8,25 +8,23 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QFileInfo srcFileInfo("./include/generated/autoconf.h");
+    QFileInfo hSrcFileInfo("./include/generated/autoconf.h");
+    QFileInfo mkSrcFileInfo("./include/autoconf.mk");
     QFile qmkFile("./autoconf.qmk");
 
     qDebug() << "generating" << qmkFile.fileName();
-    qDebug() << "from" << srcFileInfo.canonicalFilePath();
+    qDebug() << "from" << hSrcFileInfo.canonicalFilePath();
 
-    QFile autoconf(srcFileInfo.canonicalFilePath());
+    QFile autoconf_h(hSrcFileInfo.canonicalFilePath());
 
-    if (!autoconf.open(QIODevice::ReadOnly)) {
-       qDebug() << "could not open" << autoconf.fileName();
+    if (!autoconf_h.open(QIODevice::ReadOnly)) {
+       qDebug() << "could not open" << autoconf_h.fileName();
        return 1;
     }
 
     QString qmk("DEFINES += \\\n");
-    //qDebug() << QString(autoconf.readAll());
-    //qDebug() << QString(autoconf.readAll());
 
-
-    foreach (QString line, QString(autoconf.readAll()).split('\n')) {
+    foreach (QString line, QString(autoconf_h.readAll()).split('\n')) {
        if(!line.startsWith("#define"))
           continue;
        QStringList lineItems = line.split(' ');
@@ -40,6 +38,19 @@ int main(int argc, char *argv[])
        }
        qmk += " \\\n";
     }
+
+    QFile autoconf_mk(mkSrcFileInfo.canonicalFilePath());
+
+    if (autoconf_mk.open(QIODevice::ReadOnly)) {
+	    foreach (QString line, QString(autoconf_mk.readAll()).split('\n')) {
+		    if (line.contains(QRegExp("\\\".*\\\".*\\\"")))
+			    continue;
+		    line.replace("##", "");
+
+		    qmk += line + " \\\n";
+	    }
+    }
+
     qmk += "\n";
 
     if (!qmkFile.open(QIODevice::WriteOnly)) {
@@ -47,7 +58,7 @@ int main(int argc, char *argv[])
        return 3;
     }
 
-    qmkFile.write(qmk.toAscii());
+    qmkFile.write(qPrintable(qmk));
 
     return 0;
 }
